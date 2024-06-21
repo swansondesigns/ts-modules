e1cef5013f98494e1ed5a2528a89317ce265a985
Graham Swanson <graham.swanson@swansondesigns.com>
2024-06-21 11:04:21 -0600

    Color change
    Using custom colors disables the default colors.  We only use these colors once or twice so I'm just switching to custom classes.  No need to set them in the TW config file anymore.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./**/*.{htm,js}'],
-	theme: {
-		extend: {
-			colors: {
-				yellow: '#FCB540',
-				maroon: '#B04E3F',
-				green: '#B3C557',
-			},
-		},
-	},
 	plugins: [
 		function ({ addUtilities }) {
 			addUtilities({
