2fdfba8c89235ab45376823fe7166a258be94cf2
Graham Swanson <graham.swanson@swansondesigns.com>
2024-07-30 15:30:09 -0600

    Global: Add 'font-serif' class to project for Aleo.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./**/*.{htm,js}'],
+	theme: {
+		fontFamily: {
+			'serif': ['Aleo', '"Helvetica Neue"', 'Helvetica', 'Arial', 'sans-serif'],
+		},
 		extend: {
 			animation: {
 				'spin-slow': 'spin 20s linear infinite',
